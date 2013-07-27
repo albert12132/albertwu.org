bb8f1366f7b4dd57d7d07759f5ee7c21cdfc49b9
Albert Wu <albert12132@gmail.com>
2013-07-26 17:29:23 -0700

    Review: fixed typos in scheme

--- review/contents/scheme/basic/question.py
+++ review/contents/scheme/basic/question.py
@@ -9,9 +9,9 @@ title = 'Scheme'
 level = 'basic'
 
 references = [
-        'Lecture: Scheme',
-        'Lab 9',
-        'Discussion 9',
+    'Lecture: Scheme',
+    'Lab 5b',
+    'Discussion 5b',
 ]
 
 notes = ''
@@ -19,26 +19,29 @@ notes = ''
 PROMPT = 'STk> '
 
 contents = [
-        {'name': 'What would Scheme print?',
-         'id': 'print',
-         'maker': make_print_question,
-         'questions': lambda: print_questions},
-        {'name': 'Code Writing',
-         'id': 'code',
-         'maker': make_code_question,
-         'questions': lambda: code_questions},
+    {'name': 'What would Scheme print?',
+     'id': 'print',
+     'maker': make_print_question,
+     'questions': lambda: print_questions},
+    {'name': 'Code Writing',
+     'id': 'code',
+     'maker': make_code_question,
+     'questions': lambda: code_questions},
 ]
 
 print_questions = [
-    {'prompts': [
+    {
+        'prompts': [
             ('(+ 4 3)', '7'),
             ('(4 + 3)', 'Error'),
             ('(+ 1 2 3 4)', '10'),
             ('(- 1 2 3 4)', '-8'),
             ('(/ 1 2 2)', '0.25'),
         ],
-    'symbol': PROMPT},
-    {'prompts': [
+        'symbol': PROMPT
+    },
+    {
+        'prompts': [
             ('(if #t (+ 2 3))', '5'),
             ('(if #f (+ 2 3))', 'okay   ; i.e. returns nothing'),
             ('(if #f (/ 1 0) 4)', '4'),
@@ -48,8 +51,10 @@ print_questions = [
             ('(or #t #f #t)', '#t'),
             ('(or #f 4 #t)', '4'),
         ],
-        'symbol': PROMPT},
-    {'prompts': [
+        'symbol': PROMPT
+    },
+    {
+        'prompts': [
             ('(define x 2)', 'x'),
             ('x', '2'),
             ('(define (f x) (* x x))', 'f'),
@@ -58,7 +63,8 @@ print_questions = [
             ('((g 4) 5)', '20'),
         ],
         'symbol': PROMPT},
-    {'prompts': [
+    {
+        'prompts': [
             ('(cons 1 (cons 2 nil))', '(1 2)'),
             ('(cons 1 2)', '(1 . 2)'),
             ('(car (cons 1 (cons 2 nil)))', '1'),
@@ -69,12 +75,18 @@ print_questions = [
             ('(list 1 2 3 4)', '(1 2 3 4)'),
             ('(cdr (list 1 2 3 4))', '(2 3 4)'),
         ],
-        'symbol': PROMPT},
+        'symbol': PROMPT
+    },
 ]
 
 code_questions = [
-    {'description': """Implement the function <tt>filter</tt>, which takes a predicate and a Scheme list as arguments. <tt>filter</tt> will return a new list that only contains elements of the original list that satisfy the predicate.""",
-     'code': """
+    {
+        'description': """Implement the function <tt>filter</tt>,
+        which takes a predicate and a Scheme list as arguments.
+        <tt>filter</tt> will return a new list that only contains
+        elements of the original list that satisfy the predicate.""",
+
+        'code': """
 (define (filter pred lst)
     ; YOUR CODE HERE
     )
@@ -84,15 +96,21 @@ STk> (define (less-3 x) (< x 3))
 less-3
 STk> (filter less-3 (list 1 2 3 4))
 (1 2)""",
-    'solution': """
+        'solution': """
 (define (filter pred lst)
     (cond ((null? lst) nil)
           ((pred (car lst)) (cons (car lst)
                                   (filter pred (cdr lst))))
           (else (filter pred (cdr lst)))))"""
     },
-    {'description': """Implement the function <tt>interleave</tt>, which takes a two lists as arguments. <tt>interleave</tt> will return a new list that interleaves the elements of the two lists, with <tt>list1</tt> starting first. Refer to the tests for sample input/output.""",
-     'code': """
+    {
+        'description': """Implement the function <tt>interleave</tt>,
+        which takes a two lists as arguments. <tt>interleave</tt> will
+        return a new list that interleaves the elements of the two
+        lists, with <tt>list1</tt> starting first. Refer to the tests
+        for sample input/output.""",
+
+        'code': """
 (define (interleave list1 list2)
     ; YOUR CODE HERE
     )
@@ -104,7 +122,7 @@ STk> (interleave (list 1 3 5) nil)
 (1 3 5)
 STk> (interleave (list 1 3 5) (list 2 4))
 (1 2 3 4 5)""",
-    'solution': """
+        'solution': """
 (define (interleave list1 ist2)
     (if (or (null? list1) (null? list2))
         (append list1 list2)
@@ -112,8 +130,13 @@ STk> (interleave (list 1 3 5) (list 2 4))
               (cons (car list2)
                     (interleave (cdr list1) (cdr list2))))))""",
     },
-    {'description': """Implement the function <tt>count-stairways</tt>, which takes a number (of steps). Assuming we can take 1 or 2 steps, return the number of ways we can climb up the stairs.""",
-     'code': """
+    {
+        'description': """Implement the function 
+        <tt>count-stairways</tt>, which takes a number (of steps).
+        Assuming we can take 1 or 2 steps, return the number of ways
+        we can climb up the stairs.""",
+
+        'code': """
 (define (count-stairways n)
     ; YOUR CODE HERE
     )
@@ -123,7 +146,7 @@ STk> (count-stairways 4)
 5
 STk> (count-stairways 5)
 8""",
-    'solution': """
+        'solution': """
 (define (count-stairways n)
     (cond ((= n 1) 1)
           ((= n 2) 2)
--- review/contents/scheme/exam/question.py
+++ review/contents/scheme/exam/question.py
@@ -10,8 +10,8 @@ level = 'exam'
 
 references = [
     'Lecture: Scheme',
-    'Lab 9',
-    'Discussion 9',
+    'Lab 5b',
+    'Discussion 5b',
 ]
 
 notes = ''
@@ -19,41 +19,54 @@ notes = ''
 PROMPT = 'STk> '
 
 contents = [
-        {'name': 'What would Scheme print?',
-         'id': 'print',
-         'maker': make_print_question,
-         'questions': lambda: print_questions},
-        {'name': 'Code Writing',
-         'id': 'code',
-         'maker': make_code_question,
-         'questions': lambda: code_questions},
-]
-# add after the project
-[
-        {'name': 'Fill in the blank',
-         'id': 'fill',
-         'maker': make_concept_question,
-         'questions': lambda: fill_questions},
+    {'name': 'What would Scheme print?',
+     'id': 'print',
+     'maker': make_print_question,
+     'questions': lambda: print_questions},
+    {'name': 'Code Writing',
+     'id': 'code',
+     'maker': make_code_question,
+     'questions': lambda: code_questions},
+#     {'name': 'Fill in the blank',
+#      'id': 'fill',
+#      'maker': make_concept_question,
+#      'questions': lambda: fill_questions},
 ]
 
 print_questions = [
-    {'description': """For the following questions, write the value that the expression evaluates to if you type it into <tt>STk</tt>. If the expression contains a function (or multiple), write "FUNCTION" in place of that function. If the expression causes an error, write "ERROR".""",
-    'prompts': [
-        ('(cons 1 (cons 2 (cons 3 (cons 4 nil))))', '(1 2 3 4)'),
-        ('(cons (cons (cons 3 2) 1) (cons 4 nil))', '(((3 . 2) . 1) 4'),
-        ('(cdr (cons (cdr (list 1 2)) (cons 3 (cons 4 nil))))', '(3 4)'),
-        ('(define lst (cons (lambda (x) (cons x x)) nil))',),
-        ('((car lst) lst)', '((FUNCTION) FUNCTION)',),
-        ('(define (x) (lambda (x) (list x x)))',),
-        ('(((car ((x) x))) 4)', '(4 4)',),
-    ],
-    'symbol': PROMPT},
+    {
+        'description': """For the following questions, write the value
+        that the expression evaluates to if you type it into
+        <tt>STk</tt>. If the expression contains a function (or
+        multiple), write "FUNCTION" in place of that function. If the
+        expression causes an error, write "ERROR".""",
+
+        'prompts': [
+            ('(cons 1 (cons 2 (cons 3 (cons 4 nil))))', '(1 2 3 4)'),
+            ('(cons (cons (cons 3 2) 1) (cons 4 nil))', '(((3 . 2) . 1) 4'),
+            ('(cdr (cons (cdr (list 1 2)) (cons 3 (cons 4 nil))))', '(3 4)'),
+            ('(define lst (cons (lambda (x) (cons x x)) nil))',),
+            ('((car lst) lst)', '((FUNCTION) FUNCTION)',),
+            ('(define (x) (lambda (x) (list x x)))',),
+            ('(((car ((x) x))) 4)', '(4 4)',),
+        ],
+        'symbol': PROMPT
+    },
 ]
 
 code_questions = [
-    {'description': """Implement a function <tt>count-stairways</tt>, which takes a number <tt>n</tt> as an argument. Assuming you can only take 2 or 3 steps at a time, return the number of ways you can reach the top ofa staircase with <tt>n</tt> stairs.""",
-     'hint': """This is different than the usual <tt>count-stairways</tt> -- notice that this time, we can only take 2 or 3 steps, rather than 1 or 2.""",
-     'code': """
+    {
+        'description': """Implement a function
+        <tt>count-stairways</tt>, which takes a number <tt>n</tt> as
+        an argument. Assuming you can only take 2 or 3 steps at a
+        time, return the number of ways you can reach the top of a
+        staircase with <tt>n</tt> stairs.""",
+
+        'hint': """This is different than the usual
+        <tt>count-stairways</tt> -- notice that this time, we can
+        only take 2 or 3 steps, rather than 1 or 2.""",
+
+        'code': """
 (define (count-stairways n)
     ; YOUR CODE HERE
     )
@@ -67,18 +80,24 @@ STk> (count-stairways 5)
 2
 STk> (count-stairways 8)
 4""",
-    'solution': """
+        'solution': """
 (define (count-stairways n)
     (cond ((= n 1) 0)
           ((or (= n 2) (= n 3)) 1)
           (else (+ (count-stairways (- n 2))
                    (count-stairways (- n 3))))))""",
     },
-    {'description': """Implement a function <tt>count-serpinski</tt>, which takes a numbero <tt>depth</tt> as an argument. The function calculates how many triangles are contained in a Serpinski's triangle with the given depth. See the tests for examples.
-    <br/>
-<a href='http://en.wikipedia.org/wiki/File:Sierpinski_triangle_evolution.svg'>Picture</a>""",
-     'hint': """You should count triangles of all sizes.""",
-     'code': """
+    {
+        'description': """Implement a function
+        <tt>count-serpinski</tt>, which takes a numbero
+        <tt>depth</tt> as an argument. The function calculates how
+        many triangles are contained in a Serpinski's triangle with
+        the given depth. See the tests for examples.<br/>
+        <a href='http://en.wikipedia.org/wiki/File:Sierpinski_triangle_evolution.svg'>Picture</a>""",
+
+        'hint': """You should count triangles of all sizes.""",
+
+        'code': """
 (define (count-serpinski depth)
     ; YOUR CODE HERE
     )
@@ -92,30 +111,40 @@ STk> (count-stairways 3)
 17
 STk> (count-stairways 4)
 53""",
-    'solution': """
+        'solution': """
 (define (count-serpinski n)
     (cond ((= n 1) 1)
           ((= n 2) 5)
           (else (- (* 4 (count-serpinski (- n 1)))
-                   (* 3 (count-serpinski (- n 2)))))))</pre>
+                   (* 3 (count-serpinski (- n 2)))))))""",
 
-<p>The recursive call can be derived as follows:</p>
-<ol>
-    <li>(# triangles for <i>n-1</i>) + (# triangles added for this depth)</li>
-    <li>= count-serpinski(n-1) + (3/4)*4*(count-serpinski(n-2) - count-serpinski(n-2))</li>
-    <ul>
-        <li>count-serpinksi(n-1) - count-serpinski(n-2) triangles were added last time</li>
-        <li>(3/4) of the old trangles will have nested triangles</li>
-        <li>4 new triangles will be drawn in each of those (3/4) triangles</li>
-    </ul>
-    <li>= count-serpinski(n-1) + 3*count-serpinski(n-1) - 3*count-serpinski(n-2)</li>
-</ol><pre>""",
+        'explanation': """The recursive call can be derived as follows:
+        """ + ol((
+            """(# triangles for <i>n-1</i>) + (# triangles added for this
+            depth)""",
+            """= count-serpinski(n-1) + (3/4)*4*(count-serpinski(n-2) -
+            count-serpinski(n-2))""",
+            ul((
+                """count-serpinksi(n-1) - count-serpinski(n-2) triangles
+                were added last time""",
+                """(3/4) of the old trangles will have nested triangles""",
+                """4 new triangles will be drawn in each of those (3/4)
+                triangles""",
+            )),
+            """= count-serpinski(n-1) + 3*count-serpinski(n-1) -
+            3*count-serpinski(n-2)"""
+        )),
     },
 ]
 
 fill_questions = [
-    {'description': """Fill in the blanks for the <tt>construct</tt> function. It takes a value and a list of lists as arguments. It returns a new list of lists, where each element is a list from the old list, but with the value added to the front.""",
-     'code': """
+    {
+        'description': """Fill in the blanks for the
+        <tt>construct</tt> function. It takes a value and a list of
+        lists as arguments. It returns a new list of lists, where each
+        element is a list from the old list, but with the value added
+        to the front.""",
+        'code': """
 (define (construct value lists)
     (if ______
         ______
@@ -130,16 +159,24 @@ STk> (construct 6 lists)
 ((6 1) (6) (6 4 3) (6 2 5 3))
 STk> (construct 6 '())
 ()""",
-    'solution': """
+        'solution': """
 (define (construct value lists)
     (if (null? lists)
         nil
         (cons (cons value (car lists))
               (construct value (cdr lists)))))""",
     },
-    {'description': """Fill in the blanks for the <tt>list-change</tt> function. It returns a list of combinations of coins needed to make a certain amount of change (i.e. it returns a list of lists, where the elements of each list sum up to a specified value).""",
-        'hint': """The implementation of this function uses the <tt>construct</tt> function defined in Q4.""",
-     'code': """
+    {
+        'description': """Fill in the blanks for the
+        <tt>list-change</tt> function. It returns a list of
+        combinations of coins needed to make a certain amount of
+        change (i.e. it returns a list of lists, where the elements of
+        each list sum up to a specified value).""",
+
+        'hint': """The implementation of this function uses the
+        <tt>construct</tt> function defined in Q4.""",
+
+        'code': """
 (define (list-change total denoms)
     (cond ((= total 0) ______)
           ((or (< total 0) (null? denoms)) ______)
@@ -156,7 +193,7 @@ STk> (list-change 5 denoms)
 ((5) (1 1 1 1 1))
 STk> (list-change 10 denoms)
 ((10) (5 5) (5 1 1 1 1 1) (1 1 1 1 1 1 1 1 1 1))""",
-    'solution': """
+        'solution': """
 (define (list-change total denoms)
     (cond ((= total 0) (list nil))
           ((or (< total 0) (null? denomes)) nil)
