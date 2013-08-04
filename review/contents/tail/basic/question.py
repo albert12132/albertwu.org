0195bdf21ad1ef59f10483d54bdd27b85ba293af
Albert Wu <albert12132@gmail.com>
2013-08-04 15:12:25 -0700

    Review: added tail recursion questions

---
+++ review/contents/tail/basic/question.py
@@ -0,0 +1,170 @@
+from utils import utils
+from review.utils.utils import *
+
+#---------#
+# CONTENT #
+#---------#
+
+title = 'Tail Recursion'
+level = 'basic'
+
+references = [
+    'Lecture: Tail Calls, Iterators',
+    'Lab 7a',
+]
+
+notes = ''
+
+contents = [
+    {'name': 'Conceptual',
+     'id': 'conceptual',
+     'maker': make_concept_question,
+     'questions': lambda: concept_questions},
+    {'name': 'Code Writing',
+     'id': 'code',
+     'maker': make_code_question,
+     'questions': lambda: code_questions},
+]
+
+concept_questions = [
+    {
+        'description': """What is tail call optimization? What is the
+        benefit to using it?""",
+        'solution': """Tail call optimization is a
+        compiler/interpreter optimization that makes memory usage
+        more efficient for recursion. Normally, each recursive call
+        requires a new frame, and each frame needs to stick around
+        until all of the recursive calls are done. With tail call
+        optimization, once we move on to the next recursive call, the
+        old frame gets discarded, leaving only one frame in memory
+        at a time."""
+    },
+    {
+        'description': """What is a tail context, in terms of Scheme?
+        Why are tail contexts important for tail call optimization?
+        """,
+        'solution': """A tail context is a location in a Scheme
+        function that the interpreter can identify as tail recursive.
+        A Scheme function needs to be written in a particular way
+        in order for the interpreter to realize that it should perform
+        tail call optimization. The following are tail contexts:
+        """ + ul((
+            'The last sub-expression in the body of a '+ tt('lambda'),
+            'The 2nd and 3rd sub-expression in an '+ tt('if'),
+            'All non-predicate sub-expressions in a '+ tt('cond'),
+            'The last sub-expression in '+tt('and')+' or '+tt('or'),
+            'The last sub-expression in a '+tt('begin'),
+        ))
+    },
+    {
+        'description': """Which of the following Scheme functions
+        are tail recursive?""",
+        'code': """
+(define (one x)
+    (if (= x 0)
+        1
+        (* x (one (- x 1)))))
+
+(define (two x so-far)
+    (if (= x 0)
+        so-far
+        (two (- x 1) (* so-far x))))
+
+(define (three fn x)
+    (cond ((null? x) #t)
+          ((not (fn (car x))) #f)
+          (else (three fn (cdr x)))))
+
+(define (four fn x)
+    (if (null? x)
+        #t
+        (and (four fn (cdr x)) (fn x))))""",
+        'solution': ul((
+            tt('one') + ': not tail recursive',
+            tt('two') + ': tail recursive',
+            tt('three') + ': tail recursive',
+            tt('four') + ': not tail recursive',
+        ))
+    },
+]
+
+code_questions = [
+    {
+        'description': """Write a function <tt>reverse</tt> that is
+        tail recursive. It should take a list <tt>lst</tt> and return
+        the reverse of that list.""",
+        'code': """
+(define (reverse lst)
+    ; YOUR CODE HERE
+    )
+
+STk> (reverse '(1 2 3 4))
+(4 3 2 1)
+STk> (reverse nil)
+()
+""",
+        'hint': 'You should use a helper function',
+        'solution': """
+(define (reverse lst)
+    (define (reverse-help lst so-far)
+        (if (null? lst)
+            so-far
+            (reverse-help (cdr lst) (cons (car lst) so-far))))
+    (reverse-help lst nil))"""
+    },
+    {
+        'description': """Write a function <tt>reduce</tt> that is
+        tail recursive. It should take a list <tt>lst</tt>,
+        <tt>combiner</tt> function, and a <tt>start</tt> value, and
+        reduce all the elements in the list using the
+        <tt>combiner</tt>, beginnning at <tt>start</tt>.""",
+        'code': """
+(define (reduce combiner lst)
+    ; YOUR CODE HERE
+    )
+
+STk> (reduce + '(1 2 3 4) 0)
+10
+STk> (reduce - '(1 2 3 4) 0)
+-8
+STk> (reduce + '(1 2 3 4) 10)
+20
+STk> (reduce * '(1 2 3 4) 1)
+24
+""",
+        'solution': """
+(define (reduce combiner lst start)
+    (if (null? lst)
+        start
+        (reduce combiner (cdr lst) (combiner start (car lst)))))"""
+    },
+    {
+        'description': """Write a function <tt>all</tt> that is
+        tail recursive. It should take a list <tt>lst</tt> and a
+        function <tt>pred</tt>, and return true (<tt>#t</tt>) only if
+        all the elements in the list satisfy the predicate.""",
+        'code': """
+(define (all lst pred)
+    ; YOUR CODE HERE
+    )
+
+STk> (all '(1 2 3 4) even?)
+#f
+STk> (all '(1 3 5) odd?)
+#t""",
+        'solution': """
+(define (all lst pred)
+    (if (null? lst)
+        #t
+        (and (pred (car lst)) (all (cdr lst) pred))))"""
+    },
+]
+
+#-------------------#
+# COMPILING STRINGS #
+#-------------------#
+
+questions = '\n'.join(map(make_question_section, contents))
+
+attrs = globals()
+
---
+++ review/contents/tail/exam/question.py
@@ -0,0 +1,158 @@
+from utils import utils
+from review.utils.utils import *
+
+#---------#
+# CONTENT #
+#---------#
+
+title = 'Tail Recursion'
+level = 'exam'
+
+references = [
+    'Lecture: Tail Calls, Iterators',
+    'Lab 7a',
+]
+
+notes = ''
+
+contents = [
+    {'name': 'Conceptual',
+     'id': 'conceptual',
+     'maker': make_concept_question,
+     'questions': lambda: concept_questions},
+    {'name': 'Code Writing',
+     'id': 'code',
+     'maker': make_code_question,
+     'questions': lambda: code_questions},
+]
+
+concept_questions = [
+    {
+        'description': """Which of the following Scheme functions
+        are tail recursive?""",
+        'code': """
+(define (one x y)
+    (if (or (= x 0) (= y 0) (= x y))
+        1
+        (if (< x y)
+            (one x (- y x))
+            (if (> x y)
+                (one (- x y) y)))))
+
+(define (two x)
+    (if (or (= x 0) (= x 1))
+        x
+        (+ (two (- x 1)) (two (- x 2)))))
+
+(define (three n curr next)
+    (if (= n 0)
+        curr
+        (three (- n 1) next (+ curr next))))
+
+(define (four n total)
+    (cond ((= n 0) 0)
+          ((even? n) (four (- n 1) total))
+          (else (four (- n 2) (+ total n)))))
+""",
+        'solution': ul((
+            tt('one') + ': tail recursive',
+            tt('two') + ': not tail recursive',
+            tt('three') + ': tail recursive',
+            tt('four') + """: technically tail recursive, but loops
+            forever (so it's efficiently stuck)""",
+        ))
+    },
+]
+
+code_questions = [
+    {
+        'description': """Write a function <tt>map</tt> that is tail
+        recursive. It should take in a list <tt>lst</tt> and a
+        function <tt>fn</tt>, and apply the function onto every
+        element in the list.""",
+        'code': """
+(define (map fn lst)
+    ; YOUR CODE HERE
+    )
+
+STk> (map (lambda (x) (* x x)) '(1 2 3 4))
+(1 4 9 16)
+""",
+        'hint': """You should use a helper. Also, the built-in
+        <tt>append</tt> function, which concatenates two lists
+        together, should prove useful.""",
+        'solution': """
+(define (map fn lst)
+    (define (map-help lst so-far)
+        (if (null? lst)
+            so-far
+            (map-help (cdr lst)
+                      (append so-far (list (fn (car lst)))))))
+    (map-help lst nil))"""
+    },
+    {
+        'description': """Write a function <tt>filter</tt> that is
+        tail recursive. It should take in a list <tt>lst</tt> and a
+        function <tt>pred</tt>, and keep only the elements in the list
+        that satisfy the predicate.""",
+        'code': """
+(define (filter pred lst)
+    ; YOUR CODE HERE
+    )
+
+STk> (filter even? '(1 2 3 4))
+(2 4)
+""",
+        'hint': """You should use a helper. Also, the built-in
+        <tt>append</tt> function, which concatenates two lists
+        together, should prove useful.""",
+        'solution': """
+(define (filter pred lst)
+    (define (filter-help lst so-far)
+        (cond ((null? lst) so-far)
+              ((pred (car lst))
+                    (filter-help (cdr lst)
+                                 (append so-far (list (car lst)))))
+              (else (filter-help (cdr lst) so-far))))
+    (filter-help lst nil))""",
+    },
+    {
+        'description': """Write a function <tt>insert</tt> that is
+        tail recursive. It should take in a list <tt>lst</tt>, an
+        <tt>item</tt>, and an <tt>index</tt>, and insert the
+        <tt>item</tt> into the list at the given <tt>index</tt>.""",
+        'code': """
+(define (insert lst item index)
+    ; YOUR CODE HERE
+    )
+
+STk> (insert '(1 2 3 4) 100 2)
+(1 2 100 3 4)
+STk> (insert nil 10 4)
+(10)
+""",
+        'hint': """You should use a helper. Also, the built-in
+        <tt>append</tt> function, which concatenates two lists
+        together, should prove useful.""",
+        'solution': """
+(define (insert lst item index)
+    (define (insert-help lst index so-far)
+        (if (or (null? lst) (= index 0))
+            (append so-far
+                    (cons item lst))
+            (insert-help (cdr lst)
+                         (- index 1)
+                         (append so-far (list (car lst))))))
+    (insert-help lst index nil))"""
+    },
+]
+
+
+#-------------------#
+# COMPILING STRINGS #
+#-------------------#
+
+questions = '\n'.join(map(make_question_section, contents))
+
+attrs = globals()
+
