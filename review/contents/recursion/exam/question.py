cdaba872ce822ddf284a165557c8d822a0b6f615
Albert Wu <albert12132@gmail.com>
2013-09-18 13:20:03 -0700

    Review: added environment diagrams for recursion exam

--- review/contents/recursion/exam/question.py
+++ review/contents/recursion/exam/question.py
@@ -5,21 +5,23 @@ from review.utils.utils import *
 # CONTENT #
 #---------#
 
-title = 'Test'
-level = 'basic'
+title = 'Recursion'
+level = 'exam'
 
 references = [
-    'Reference 1',
-    'Reference 2',
+    'Lecture: Recursion',
+    'Lecture: Tree Recursion',
+    'Lab 3',
+    'Discussion 3',
 ]
 
 notes = ''
 
 contents = [
-    {'name': 'Conceptual',
-     'id': 'conceptual',
-     'maker': make_concept_question,
-     'questions': lambda: concept_questions},
+    {'name': 'What would Python print?',
+     'id': 'print',
+     'maker': make_print_question,
+     'questions': lambda: print_questions},
     {'name': 'Environment Diagrams',
      'id': 'env',
      'maker': make_env_question,
@@ -28,26 +30,6 @@ contents = [
      'id': 'code',
      'maker': make_code_question,
      'questions': lambda: code_questions},
-    {'name': 'What would Python print?',
-     'id': 'print',
-     'maker': make_print_question,
-     'questions': lambda: print_questions},
-    {'name': 'Eval vs. Display',
-     'id': 'eval_print',
-     'maker': make_eval_print_question,
-     'questions': lambda: eval_print_questions},
-]
-
-concept_questions = [
-    {
-        'description': """Question Description.""",
-        'code': """
-def foo(test):
-    return 'this is a test'
-""",
-        'hint': 'This is a hint',
-        'solution': 'hi'
-    },
 ]
 
 print_questions = [
@@ -75,20 +57,43 @@ def foo(test):
 env_questions = [
     {
         'code': """
-def code(test):
-    return test
+def factorial(n):
+    if n == 0:
+        return 1
+    return n * factorial(n-1)
+
+ans = factorial(2)
+"""
+    },
+    {
+        'code': """
+def foo(n):
+    i = 0
+    if n == 0:
+        return 0
+    result = foo(n - 2)
+    i += 1
+    return i + result
+
+result = foo(4)
+"""
+    },
+    {
+        'code': """
+def bar(f):
+    def g(x):
+        if x == 1:
+            return f(x)
+        else:
+            return f(x) + g(x - 1)
+    return g
+
+f = 4
+bar(lambda x: x + f)(2)
 """
     },
 ]
 
-eval_print_questions = [
-    {
-        'description': 'This is a description',
-        'prompts': [
-            ('x + 2', 'evaluates', 'displays'),
-        ]
-    },
-]
 
 #-------------------#
 # COMPILING STRINGS #
