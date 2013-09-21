3809c4c45acce74e54ff6501d99512f37b178b3f
Albert Wu <albert12132@gmail.com>
2013-09-21 15:41:02 -0700

    Review: added newtons question

--- review/contents/newtons/exam/question.py
+++ review/contents/newtons/exam/question.py
@@ -17,68 +17,73 @@ iterative improvement from lecture:""" + \
 pre("""
 # iterative improvement framework
 
-def improve(update, isclose, guess=1):
-    while not isclose(guess):
+def improve(update, close, guess=1):
+    while not close(guess):
         guess = update(guess)
     return guess
 
-def approx_eq(x, y, tolerance=1e-3):
+def approx_eq(x, y, tolerance=1e-5):
     return abs(x - y) < tolerance
 
 # Newton's method
 
-def approx_derivative(f, x, delta=1e-5):
-    df = f(x + delta) - f(x)
-    return df / delta
+def make_derivative(f, delta=1e-5):
+    def derivative(x):
+        df = f(x + delta) - f(x)
+        return df / delta
+    return derivative
 
-def newton_update(f):
+def newton_update(f, df):
     def update(x):
-        return x - f(x) / approx_derivative(f, x)
+        return x - f(x) / df(x)
     return update
 
-def find_root(f, initial_guess=10):
-    def close_to_zero(x):
+def find_zero(f, df):
+    def near_zero(x):
         return approx_eq(f(x), 0)
-    return improve(newton_update(f), close_to_zero, initial_guess)""",
+    return improve(newton_update(f, df), near_zero)""",
 classes='prettyprint')
 
 contents = [
-#         {'name': 'Conceptual',
-#          'id': 'conceptual',
-#          'maker': make_concept_question,
-#          'questions': lambda: concept_questions},
-        {'name': 'Code Writing',
-         'id': 'code',
-         'maker': make_code_question,
-         'questions': lambda: code_questions},
+    # {'name': 'Conceptual',
+    #  'id': 'conceptual',
+    #  'maker': make_concept_question,
+    #  'questions': lambda: concept_questions},
+    {'name': 'Code Writing',
+     'id': 'code',
+     'maker': make_code_question,
+     'questions': lambda: code_questions},
 ]
 
 concept_questions = [
-    {'description': """Question Description.""",
-     'code': """
+    {
+        'description': """Question Description.""",
+        'code': """
 def foo(test):
     return 'this is a test'
 """,
-    'solution': 'hi'
+        'solution': 'hi'
     },
 ]
 
 code_questions = [
-        {'description': """Using Newton's method, write a function
+    {
+        'description': """Using Newton's method, write a function
         <tt>fourth_root</tt> that calculates the fourth root of a
         number <tt>x</tt>. <i>Note</i>: this is not quite the same as
         finding the root of the fourth root (since that's just <tt>x =
         0</tt>).
         """,
-     'code': """
+        'code': """
 def fourth_root(x):
     "*** YOUR CODE HERE ***" """,
     'solution': """
 def fourth_root(x):
     f = lambda y: y**4 - x
-    return find_root(f)"""
+    return find_zero(f, make_derivative(f))"""
     },
-        {'description': """An elementary exercise in calculus is to
+    {
+        'description': """An elementary exercise in calculus is to
         find a critical point of a function. The <a href='http://en.wikipedia.org/wiki/Critical_point_(mathematics)'>critical point</a>
         of a mathematical function <i>f</i> is a value <i>x</i> such
         that the <i>derivative of f</i> at <i>x</i> is equal to 0 (i.e.
@@ -88,16 +93,38 @@ def fourth_root(x):
         <p>Write a function <tt>critical_point</tt> that takes a
         function <tt>f</tt> and returns a critical point for that
         function.""",
-     'code': """
+        'code': """
 def critical_point(f):
     \"\"\"Returns a single critical point for the function F.\"\"\"
     "*** YOUR CODE HERE ***" """,
-    'solution': """
+        'solution': """
 def critical_point(f):
-    f_prime = lambda x: approx_derivative(f, x)
-    return find_root(f_prime)"""
+    df = make_derivative(f)
+    ddf = make_derivative(df)
+    return find_root(df, ddf)"""
     },
-        {'description': """In economics, <i><a href='http://en.wikipedia.org/wiki/Walrasian_auction'>t&acirc;tonnement</a></i> is an iterative process for finding price equilibria in a market. The price
+    {
+        'description': """If Newton's method reaches a guess that has
+        a slope of 0 (also known as a <a href='http://en.wikipedia.org/wiki/Critical_point_(mathematics)'>critical point</a>), then
+        <tt>newton_update</tt> will raise a ZeroDivisionError (i.e.
+        the derivative of the function = 0). Rewrite
+        <tt>newton_update</tt> to add a small <tt>offset</tt> to the
+        derivative if it is equal to zero to avoid this problem.""",
+        'code': """
+def newton_update(f, df, offset=1e-5):
+    "*** YOUR CODE HERE ***" """,
+        'solution': """
+def newton_update(f, df, offset=1e-5):
+    def update(x):
+        deriv = df(x)
+        if deriv == 0:
+            deriv += offset
+        return x - f(x) / deriv
+    return update
+"""
+    },
+    {
+        'description': """In economics, <i><a href='http://en.wikipedia.org/wiki/Walrasian_auction'>t&acirc;tonnement</a></i> is an iterative process for finding price equilibria in a market. The price
         of a good is adjusted depending on the amount of excess
         demand until demand is equal to supply. The price at which
         this happens is called the equilibrium price. Here is one
@@ -114,7 +141,7 @@ def critical_point(f):
         a price as an argument and output a quantity), and returns
         an approximation of the equilibrium price. See the docstring
         for more details.""",
-     'code': """
+        'code': """
 def equilibrium(supply, demand, tolerance=1e-2):
     \"\"\"Calculates the equilibrium price for the given SUPPLY and
     DEMAND functions.
@@ -134,12 +161,12 @@ def equilibrium(supply, demand, tolerance=1e-2):
     def update(p):
         "*** YOUR CODE HERE ***"
 
-    def isclose(p):
+    def close(p):
         "*** YOUR CODE HERE ***"
 
     return improve(update, isclose, initial_guess)
 """,
-    'solution': """
+        'solution': """
 def equilibrium(supply, demand, tolerance=1e-2):
     initial_guess = 1
 
