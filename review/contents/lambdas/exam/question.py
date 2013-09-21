a00dc1834d3a071e2df94d31d86faa05bd13a68b
Albert Wu <albert12132@gmail.com>
2013-09-21 15:04:50 -0700

    Review: added some lambdas not involving lists

--- review/contents/lambdas/exam/question.py
+++ review/contents/lambdas/exam/question.py
@@ -28,22 +28,40 @@ contents = [
 code_questions = [
     {
         'description': """Fill in the blanks for the following
-        expression so that <tt>result</tt> is a list.""",
+        expression so that <tt>result</tt> is the number 42.""",
         'code': """
-x = lambda x, y: lambda: [x, y]
-result = (lambda ____, game: fun(_____)_____)(x, (3, 2))""",
+x = lambda x, y: lambda: x - y
+result = (lambda ____, question: one(__________)(x, 4)""",
     'solution': """
-result = (lambda fun, game: fun(game[0], game[1])())(x, (3, 2))"""
+result = (lambda one, question: one(46, question)())(x, 4)"""
     },
     {
         'description': """Fill in the blanks for the following
-        expression so that <tt>result</tt> is the number 3.""",
+        expression so that <tt>result</tt> is the boolean True.""",
         'code': """
-f = lambda: lambda x: x[0]
-result = (lambda _____: f(_____)(_____))(lambda: [3])""",
+x = lambda x: lambda y: x(y)
+result = (lambda ______: x(fair)(dice))(lambda fair: fair == 3, 3)""",
     'solution': """
-result = (lambda var: f()(var()))(lambda: [3])"""
+result = (lambda fair, dice: x(fair)(dice))(lambda fair: fair == 3, 3)"""
     },
+#     {
+#         'description': """Fill in the blanks for the following
+#         expression so that <tt>result</tt> is a list.""",
+#         'code': """
+# x = lambda x, y: lambda: [x, y]
+# result = (lambda ____, game: fun(_____)_____)(x, (3, 2))""",
+#     'solution': """
+# result = (lambda fun, game: fun(game[0], game[1])())(x, (3, 2))"""
+#     },
+#     {
+#         'description': """Fill in the blanks for the following
+#         expression so that <tt>result</tt> is the number 3.""",
+#         'code': """
+# f = lambda: lambda x: x[0]
+# result = (lambda _____: f(_____)(_____))(lambda: [3])""",
+#     'solution': """
+# result = (lambda var: f()(var()))(lambda: [3])"""
+#     },
     {
         'description': """Fill in the blanks for the following
         expression so that each call to <tt>mapper</tt> prints the
