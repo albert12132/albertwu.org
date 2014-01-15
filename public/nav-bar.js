d5f0169bdd7bf9bd71379588f4e32f68a6dee0a7
Albert Wu <albert12132@gmail.com>
2014-01-14 21:07:27 -0800

    Modifying styles for nav bar and 404

---
+++ public/nav-bar.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  var navBarWidth = '270px';
+  var navBarSpeed = 500;
+  $('#nav-bar-hide').click(function(){
+    if ($('#content').css('margin-left') === navBarWidth) {
+      $('#content').animate({'margin-left':0}, navBarSpeed);
+    } else {
+      $('#content').animate({'margin-left':navBarWidth}, navBarSpeed);
+    }
+  });
+});
+
+$(window).resize(function(){
+  $('#nav-bar').css('height', ($(window).height() - 100) + 'px');
+});
