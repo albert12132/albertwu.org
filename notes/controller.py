8ee8ade90b03764d34d3fddc347b26cfce242042
Albert Wu <albert12132@gmail.com>
2014-05-21 18:59:31 -0700

    Update templar and add table of contents to notes

--- notes/controller.py
+++ notes/controller.py
@@ -8,6 +8,25 @@ def style_guide_sub(match):
     elif style == 'P':
         return '<span class="python">P</span>'
 
+def toc(lst):
+    if not lst:
+        return ''
+    cur = 0
+    text = ''
+    for level, tag, title in lst:
+        level = int(level)
+        if level > cur:
+            text += '<ul>\n'
+        elif level < cur:
+            text += '</ul>\n'
+        text += '<li><a href="#{0}">{1}</a></li>\n'.format(tag, title)
+        if level != cur:
+            cur = level
+    if cur != lst[0][0]:
+        text += '</ul>\n'
+    text += '</ul>\n'
+    return text
+
 regexes = [
     (style_guide_re, style_guide_sub),
 ]
