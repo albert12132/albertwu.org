098e47ccea13c1362d67fb07b83cce694c9738a5
Albert Wu <albert12132@gmail.com>
2014-06-08 21:13:10 -0700

    Update example app

--- .example_app/README.md
+++ .example_app/README.md
@@ -4,14 +4,6 @@ Example App
 This directory contains a skeleton for a new app. Rewrite this README
 to explain the app.
 
-Every app requires the following:
-
-* `__init__`: allows the compiler to acces content files in this
-  directory as Python modules
-* `Makefile`: configure this for more user friendly compiler interface
-* `templates`: a directory that houses templates for this app
-
-
 Makefile
 --------
 
---
+++ .example_app/config.py
@@ -0,0 +1,41 @@
+import os
+import re
+
+# Path of the current file -- best not to change this
+FILEPATH = os.path.dirname(os.path.abspath(__file__))
+
+#############
+# Templates #
+#############
+
+# List of directories in which to search for templates
+TEMPLATE_DIRS = [
+    FILEPATH,
+    # Add directories that contain templates
+    # os.path.join(FILEPATH, 'example'),
+]
+
+# Variables that can be used in templates
+VARIABLES = {
+    # Add variables here, like the following
+    # 'example': 'something here',
+}
+
+#################
+# Substitutions #
+#################
+
+# Substitutions for the linker
+SUBSTITUTIONS = [
+    # Add substitutinos of the form
+    # (regex, sub_function),
+]
+
+
+# Use the following to scrape "headers"
+# header_regex = re.compile(r"")
+# def header_translate(match):
+#     pass
+# def table_of_contents(headers):
+#     pass
+
